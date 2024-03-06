e38485d06e1d9d16b54159b9ff68d95860bb96cf
Gabriel Gandur <grgandur@gmail.com>
2024-03-06 11:47:40 -0300

    Estudando animações em SwiftUI

---
+++ Landmarks/Model/Hike.swift
@@ -0,0 +1,37 @@
+//
+//  Hike.swift
+//  Landmarks
+//
+//  Created by Gabriel Rocha Gandur on 04/03/24.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import Foundation
+
+struct Hike: Codable, Hashable, Identifiable {
+    var id: Int
+    var name: String
+    var distance: Double
+    var difficulty: Int
+    var observations: [Observation]
+
+
+    static var formatter = LengthFormatter()
+
+
+    var distanceText: String {
+        Hike.formatter
+            .string(fromValue: distance, unit: .kilometer)
+    }
+
+
+    struct Observation: Codable, Hashable {
+        var distanceFromStart: Double
+
+
+        var elevation: Range<Double>
+        var pace: Range<Double>
+        var heartRate: Range<Double>
+    }
+}
+
